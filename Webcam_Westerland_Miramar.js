aa89740b7d5567acbda7369e5ba7bf6176da8510
JLuetzen <76537507+JLuetzen@users.noreply.github.com>
2021-01-31 16:48:07 +0100

    Update Webcam_Westerland_Miramar.js
    Syntax corrections

--- Webcam_Westerland_Miramar.js
+++ Webcam_Westerland_Miramar.js
@@ -8,45 +8,45 @@
 
 
 
-const camLocation = "Westerland Hotel Miramar"
-const camSubtitle = "Mein Bild 1.0"
-const url = "https://livestream.nordwind-ev.de/westerland-03.html"
-const imgUrl = "https://www.worldcam.pl/images/webcams/420x236/strand-sylt.jpg"
-const imgReq = await new Request(imgUrl)
-const img = await imgReq.loadImage()
+const camLocation = "Westerland Hotel Miramar";
+const camSubtitle = "Mein Bild 1.0";
+const url = "https://livestream.nordwind-ev.de/westerland-03.html";
+const imgUrl = "https://www.worldcam.pl/images/webcams/420x236/strand-sylt.jpg";
+const imgReq = await new Request(imgUrl);
+const img = await imgReq.loadImage();
 
 if (config.runsInWidget) {
-  let widget = createWidget(img)
-  Script.setWidget(widget)
-  Script.complete()
+  let widget = createWidget(img);
+  Script.setWidget(widget);
+  Script.complete();
 } else { 	
-  let widget = createWidget(img)
-  await widget.presentMedium()
+  let widget = createWidget(img);
+  await widget.presentMedium();
 }
 
 function createWidget(img) {
 
-  const widget = new ListWidget()
-  widget.backgroundColor = Color.black()
-  widget.url = url
-  widget.backgroundImage = img 
+  const widget = new ListWidget();
+  widget.backgroundColor = Color.black();
+  widget.url = url;
+  widget.backgroundImage = img;
 
-  widget.addSpacer()
+  widget.addSpacer();
 
-  const titleText = widget.addText(camLocation)
-  titleText.font = Font.boldSystemFont(12)
-  titleText.textColor = Color.white()
-  titleText.shadowRadius = 3
-  titleText.shadowColor = Color.black()
+  const titleText = widget.addText(camLocation);
+  titleText.font = Font.boldSystemFont(12);
+  titleText.textColor = Color.white();
+  titleText.shadowRadius = 3;
+  titleText.shadowColor = Color.black();
 
-  widget.addSpacer(2)
+  widget.addSpacer(2);
 
-  const subtitleText = widget.addText(camSubtitle)
-  subtitleText.font = Font.systemFont(8)
-  subtitleText.textColor = Color.white()
-  subtitleText.textOpacity = 0.8
-  subtitleText.shadowRadius = 3
-  subtitleText.shadowColor = Color.black()
+  const subtitleText = widget.addText(camSubtitle);
+  subtitleText.font = Font.systemFont(8);
+  subtitleText.textColor = Color.white();
+  subtitleText.textOpacity = 0.8;
+  subtitleText.shadowRadius = 3;
+  subtitleText.shadowColor = Color.black();
 
   return widget
 }
