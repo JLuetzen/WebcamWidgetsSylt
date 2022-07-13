51801f40200ab2718c2442a54740df982ad40153
JLuetzen <Kliffkieker67@gmail.com>
2022-07-13 20:21:21 +0200

    Create WWS_Update_Surfcams_Bayern.js

---
+++ data/WWS_Update_Surfcams_Bayern.js
@@ -0,0 +1,63 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: camera-retro;
+// Script by Jens Luetzen: <kliffkieker67@gmail.com>
+
+//
+// Initialization der Variablen
+//
+// Diese beiden Konstanten sind die Minimalwerte zum Laden des Parameterfiles
+///
+const GitHubDataPath = 'https://wwsylt.live/data';
+const GitHubMasterScriptFile = "WWS_Master_singles"
+
+//const localWidgetfile = "WWS_CSML-Edition"
+// var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,localWidgetfile+".js");
+//
+// WWS_SyltcamsSyltCams
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Brombachsee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Brombachsee_Video.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Altmühlsee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Chiemsee Übersee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Chiemsee Übersee Video.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Chiemsee Prienavera.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Tegernsee Wallberg.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Tergernsee Sonnenbichl.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Ammersee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Walchensee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Kochelsee.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Starnbergersee Ammerland.js");
+var scriptfile = await copyFile(GitHubMasterScriptFile+".js",GitHubDataPath,"WWS_BAY_Gardasee Malcesine.js");
+
+Script.complete();
+
+
+
+
+//
+// get scriptfile as download
+//
+async function copyFile(prvmasterscriptfile, prvscriptfileURL, prvlocalwidgetfile) {
+    let fm = FileManager.iCloud();
+    let dir = fm.documentsDirectory();
+    let path = fm.joinPath(dir, prvlocalwidgetfile);
+
+		console.log("scriptfile Title = " + prvlocalwidgetfile);
+
+		// erst Löschen der bisherigen Datei (ohne Datumsstring)
+		if (fm.fileExists(path)) {
+			 console.log("Lösche Datei")
+			fm.remove(path);
+		}
+
+		let writepath = fm.joinPath(dir, prvlocalwidgetfile);
+		let scriptUrl;
+		scriptUrl = prvscriptfileURL + "/" + prvmasterscriptfile;
+		let req = new Request(scriptUrl);
+		let transferfile = await req.loadString();
+		fm.writeString(writepath,transferfile);
+		console.log("Schreibe neue Datei: " + writepath);
+
+		return scriptfile;
+}
+/* closes function getData */
