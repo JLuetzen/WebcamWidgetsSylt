054af9981c2e476df02a57c27f67cd18baf984ca
JLuetzen <Kliffkieker67@gmail.com>
2022-08-09 21:38:56 +0200

    Update Names

--- data/WWSmod_Parameters_singles.js
+++ data/WWSmod_Parameters_singles.js
@@ -39,7 +39,7 @@ switch (myFilename) {
 		CamFamily = "Sylt Cams";
 		GitHubCamFile = "Cams_Sylt.txt";
 		sourceURL = "https://wwsylt.live";
-		GitHubLogoName = 'WWS_Logo_Westerland 1 farbig.png';
+		GitHubLogoName = 'WWS_Logo_Westerland_1_farbig.png';
 		GitHubErrorPic = 'Error_Parameter.png';
 		GitHubSpecialCaseFile = 'WWSmod_Specialcases';
 		hinweistext = "";
@@ -95,7 +95,7 @@ switch (myFilename) {
 		CamFamily = "Sylt Cams";
 		GitHubCamFile = "Cams_Sylt.txt";
 		sourceURL = "https://wwsylt.live";
-		GitHubLogoName = 'WWS_Logo_Westerland 1 weiss';
+		GitHubLogoName = 'WWS_Logo_Westerland_1_weiss';
 		GitHubErrorPic = 'Error_Parameter.png';
 		GitHubSpecialCaseFile = 'WWSmod_Specialcases';
 		hinweistext = "";
