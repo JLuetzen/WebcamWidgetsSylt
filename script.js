856a432160c2034979c90ca77d2668a39edd5dba
JLuetzen <76537507+JLuetzen@users.noreply.github.com>
2023-03-17 21:11:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let processScroll =() => {
         scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
         scrollPercent = scrollTop / scrollBottom * 100 + '%';
 
+console.log('Top: ' + scrollTop + ' / Bottom: ' + scrollBottom + ' / InnerHeight: ' + window.innerHeight)
+
     document.getElementById('progress-bar').style.setProperty('--scrollAmount', scrollPercent);
 }
 
